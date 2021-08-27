c337a77f697c5d0135b5b70f58bed3c438883d3c
James Brotsos <j.brotsos@gmail.com>
2021-08-27 10:56:14 -0700

    Create login.java

---
+++ login.java
@@ -0,0 +1,16 @@
+String username = request.getParameter("username");
+String password = request.getParameter("password");
+
+String sql = "select * from users where (username ='" + username +"' and password ='" + password + "')";
+
+Connection connection = pool.getConnection();
+Statement statement = connection.createStatement();
+ResultSet result = statement.executeQuery(sql);
+
+if (result.next()) {
+    loggedIn = true;
+    // Successfully logged in and redirect to user profile page
+
+} else {
+    // Auth failure - Redirect to Login Page
+}
