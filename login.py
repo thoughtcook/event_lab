09bc2e08d57940cf9140ca334770f87796780f27
James Brotsos <j.brotsos@gmail.com>
2021-08-27 14:57:59 -0700

    Update login.py

--- login.py
+++ login.py
@@ -1,25 +1,18 @@
+#from django.http import HttpResponse
+from django.shortcuts import render, HttpResponse, redirect, HttpResponseRedirect, reverse
+
 def authenticate(request):
-3
     email = request.POST['email']
-4
     password = request.POST['password']
-5
     sql = "select * from users where (email ='" + email + "' and password ='" + password + "')"
-6
-​
-7
+
     cursor = connection.cursor()
-8
     cursor.execute(sql)
-9
+
     row = cursor.fetchone()
-10
     if row:
-11
         loggedIn = "Logged Success"
-12
     else:
-13
         loggedIn = "Login Failure"
-14
+        
     return HttpResponse("Logged In Status:" + loggedIn)
