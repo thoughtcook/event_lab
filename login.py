9997bc6ae1825d5397ec904562b13e7e41b79775
James Brotsos <j.brotsos@gmail.com>
2021-08-29 16:03:50 -0700

    Update login.py

--- login.py
+++ login.py
@@ -1,3 +1,19 @@
-# Do NOT do it this way.
-cmd = "update people set name='%s' where id='%s'" % (name, id)
-curs.execute(cmd)
+import requests
+from django.http import HttpResponse
+from sqlite3 import *
+
+email = requests.POST['email']
+password = requests.POST['password']
+
+sql = "select * from users where (email ='" + email + "' and password ='" + password + "')"
+
+cursor = connection.cursor()
+cursor.execute(sql)
+row = cursor.fetchone()
+if row:
+  loggedIn = "Logged Success"
+else:
+  loggedIn = "Login Failure"
+        
+return HttpResponse("Logged In Status:" + loggedIn)
+ 
