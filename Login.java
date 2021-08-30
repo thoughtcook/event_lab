6dbd91231e5e7b6b847686f7237bd279c1958bd9
James Brotsos <j.brotsos@gmail.com>
2021-08-29 17:09:47 -0700

    Update and rename login.java to Login.java

---
+++ Login.java
@@ -0,0 +1,37 @@
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.jsp.*;
+import cnt.Security.*;
+
+import java.lang.StringBuffer;
+import java.sql;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.ResultSet;
+
+import javax.servlet.http.HttpServletRequest;
+
+// Class Declaration
+class Login
+{
+    public static void main(String[] args)   
+    {
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+
+        String sql = "select * from users where (email ='" + email +"' and password ='" + password + "')";
+
+        Connection connection = pool.getConnection();
+        Statement statement = connection.createStatement();
+        ResultSet result = statement.executeQuery(sql);
+
+        if (result.next()) {
+            loggedIn = true;
+            // Successfully logged in and redirect to user profile page
+        } else {
+            // Auth failure - Redirect to Login Page
+        }
+    }
+} 
